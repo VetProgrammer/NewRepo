3516c6b19f5abc2cdd8b94e4f31ebcda17c83a91
VetProgrammer <tedt8018@gmail.com>
2023-02-13 16:36:45 -0500

    Add project files.

---
+++ DepInjDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DepInjDemoMVC.Models;
+using DepInjDemoMVC.Services.NonDatabase;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DepInjDemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+       // private readonly ILogger<HomeController> _logger;
+        private readonly IEmailSenderService _service;
+        public HomeController(IEmailSenderService service)
+        {
+                 _service = service;
+            
+        }
+
+        public IActionResult Index()
+        {
+            _service.SendEmail("Hello World");
+            ViewData["Message"] = _service.Message;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DepInjDemoMVC/Services/NonDatabase/EmailSenderService.cs
@@ -0,0 +1,18 @@
+﻿namespace DepInjDemoMVC.Services.NonDatabase
+{
+    public interface IEmailSenderService
+    {
+        void SendEmail(string email); 
+        string Message { get; set; }
+    }
+    public class EmailSenderService: IEmailSenderService
+    {
+        // Declare a constructor for a Message to be passed to the controller and the index.cshtml
+        public string Message { get; set; } = ""; 
+       public void SendEmail(string email) {
+            Message = email;
+           // Console.WriteLine(email);
+
+        }      
+    }
+}
